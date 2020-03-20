c96f73afc2b0db82285f77b0ae380df9c119d576
Graham Pierce <grahampiercenyc@gmail.com>
2020-03-20 17:28:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  puts [0]
+  puts [0];
 end
 
 def retrieve_last_element_from_array(array)
